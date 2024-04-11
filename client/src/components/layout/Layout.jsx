5972e6a7ad025f0afab86b047c96bd3b1df11a12
vatsal-bhatti <vatsal.bhatti@bacancy.com>
2024-04-11 23:44:41 +0530

    final commit all commits are done

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -14,49 +14,34 @@ function Layout() {
   const loginState = useSelector((state) => state.registerLoginReducer);
   const dispatch = useDispatch();
 
-
-useEffect(()=>{
-  const storedUserDetails =
-  JSON.parse(localStorage.getItem("userDetails")) || {};
-  dispatch(
-    setRole(
-      storedUserDetails.roleDetails,
-      storedUserDetails.role,
-      storedUserDetails.isAuth
-    )
-  );
-
-},[])
-
+  useEffect(() => {
+    const storedUserDetails =
+      JSON.parse(localStorage.getItem("userDetails")) || {};
+    dispatch(
+      setRole(
+        storedUserDetails.roleDetails,
+        storedUserDetails.role,
+        storedUserDetails.isAuth
+      )
+    );
+  }, []);
 
   useEffect(() => {
     // Retrieve user details from local storage
-    
+
     setUserDetails(loginState);
     setIsAuth(loginState.isAuth);
     setRole(loginState.role);
     setRoleDetails(loginState.roleDetails);
-   
   }, [loginState]);
 
   // const loginState = useSelector((state) => state.registerLoginReducer);
   // console.log(loginState);
-console.log(loginState)
+  console.log(loginState);
   return (
     <div>
       <Navbar role={role} isAuth={isAuth} />
       <Outlet />
-      {/* Uncomment or remove components as needed */}
-      {/* <MainPage /> */}
-      {/* <HackathonsPage/> */}
-      {/* <HostHackathonsPage/> */}
-      {/* <AdminMainPage/> */}
-      {/* <ParticipantProfile /> */}
-      {/* <ViewDetailsPage/> */}
-      {/* <MainListPage/> */}
-      {/* <RegisterLogin login={false}/> */}
-      {/* <AddHackathonForm/> */}
-      {/* <ApplyNow/> */}
       <Footer id="footer" />
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createStore ,applyMiddleware} from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/rootreducer.jsx";
 // import {thunk } from 'redux-thunk'
-import {thunk} from "redux-thunk"
+import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { BrowserRouter } from "react-router-dom";
 import { Button } from "./components/common/Button.jsx";
@@ -14,17 +14,15 @@ import "./fonts/Nunito_Sans/NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf";
 
 // import HackathonsPage from "./components/layout/HackathonsPage/HackathonsPage.jsx";
 
-
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      
-        <App />
-
-      {/* <HackathonsPage/> */}
+      <App />
     </Provider>
   </React.StrictMode>
 );
